33db361ae8eae296fa14b067f0243cbe3cdc8c3e
yashjangir04 <jangiryash04@gmail.com>
2025-05-29 12:31:18 +0530

    Changed port for socket

--- Controllers/coderunner.js
+++ Controllers/coderunner.js
@@ -123,6 +123,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log("Socket.io server running on port 3001");
+server.listen(3000, () => {
+  console.log("Socket.io server running on port 3000");
 });
